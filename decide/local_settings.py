31c3ea0ceca6a8edf3b5ce8861d63428acddfb58
JAVIER MARTÍNEZ FERNÁNDEZ <javmarfer3@alum.us.es>
2021-11-20 00:59:06 +0100

    fix: Añade configuración inicial
    Se ha añadido la configuración para conectar la base de datos correctamente.
    Se está trabajando en un error dado con respecto a la zona horaria.

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -32,7 +32,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'decide',
-	'PASSWORD': 'practica',
+	'PASSWORD': 'decide',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
