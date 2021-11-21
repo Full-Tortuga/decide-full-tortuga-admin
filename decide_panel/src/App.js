6ac6ba8e142ec8e4fd82d50f305fa376d7287204
JAVIER MARTÍNEZ FERNÁNDEZ <javmarfer3@alum.us.es>
2021-11-21 10:40:39 +0100

    feat: Añade primera conexión
    Se ha implementado una conexión de prueba del front-end con React con el back-end de decide. Además, se han instalado diversas librerías que se usarán en el proyecto.

--- decide_panel/src/App.js
+++ decide_panel/src/App.js
@@ -1,10 +1,47 @@
-import "./App.css";
+import "./css/App.css";
+import { Card } from "@nextui-org/react";
+import React from "react";
+import axios from "axios";
+import { useRef } from "react";
+import { Messages } from "primereact/messages";
 
 function App() {
+  const messages = useRef(null);
+
+  function connect() {
+    axios
+      .get("http://localhost:8000/admin/")
+      .then((res) => {
+        if (res.status === 200) {
+          messages.current.show({
+            severity: "success",
+            summary: "Conexión exitosa",
+          });
+        }
+      })
+      .catch((err) => {
+        messages.current.show({
+          severity: "warn",
+          summary: "Conexión fallida",
+        });
+      });
+  }
+
   return (
-    <div>
-      <header>Hola</header>
-    </div>
+    <React.Fragment>
+      <Messages ref={messages}></Messages>
+      <Card
+        id="principal"
+        color="gradient"
+        textColor="white"
+        width="50%"
+        hoverable="true"
+        onClick={connect}
+        clickable="true"
+      >
+        Conectar con decide (prueba de conexión)
+      </Card>
+    </React.Fragment>
   );
 }
 
--- decide_panel/src/index.js
+++ decide_panel/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { CssBaseline } from "@nextui-org/react";
+import "primeflex/primeflex.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primereact/resources/themes/bootstrap4-dark-blue/theme.css";
+import PrimeReact from "primereact/api";
+import { Suspense } from "react";
+
+PrimeReact.ripple = true;
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Suspense fallback="Loading">
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Suspense>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
