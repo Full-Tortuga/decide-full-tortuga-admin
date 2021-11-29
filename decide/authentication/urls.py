05628768a1a8510d6654f76f8db1133b02212cb1
pedalopon <pedropontiga@gmail.com>
2021-11-29 20:16:49 +0100

    #6-feat:Added user CRUD operations

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from .views import GetUserView, LogoutView, RegisterView
+from .views import *
 
 
 urlpatterns = [
@@ -9,4 +9,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view()),
     path('getuser/', GetUserView.as_view()),
     path('register/', RegisterView.as_view()),
+    path('users/', UsersAPI.as_view()),
+    path('user/', UserAPI.as_view()),
+
 ]
