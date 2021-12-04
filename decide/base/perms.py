d6dac57a6e4be011a9ea275b957d632a50a0e420
pedalopon <pedropontiga@gmail.com>
2021-12-04 19:00:12 +0100

    #6-feat:Added IsAdminAPI permission

--- decide/base/perms.py
+++ decide/base/perms.py
@@ -1,4 +1,7 @@
-from rest_framework import permissions
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions, exceptions
+from rest_framework.authtoken.models import Token
 
 from base import mods
 
@@ -9,5 +12,17 @@ class UserIsStaff(permissions.BasePermission):
         if not request.auth:
             return False
         response = mods.post('authentication/getuser', json={'token': request.auth.key},
-                response=True)
+                             response=True)
         return response.json().get('is_staff', False)
+
+
+class IsAdminAPI(permissions.BasePermission):
+    message = 'Adding customers not allowed.'
+
+    def has_permission(self, request, view):
+        key = request.COOKIES.get('token', "")
+        try:
+            tk = get_object_or_404(Token, key=key)
+        except Http404:
+            raise exceptions.PermissionDenied(detail="You are not logged in")
+        return tk.user.is_superuser
