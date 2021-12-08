997be8050ef107d8d385cb280a0bd6a23487b416
josaloroc <joseluisalro20@gmail.com>
2021-12-08 18:08:23 +0100

    Added Auth and Key CRUD Operations for Base module
    Implemented CRUD Operations for Auth and Key

--- decide/administration/views.py
+++ decide/administration/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from base import serializers
+from base.mods import query
 from rest_framework.status import *
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
@@ -7,14 +9,110 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from base.models import Auth, Key
+from rest_framework.renderers import JSONRenderer
 
 from authentication.serializers import UserSerializer
+from base.serializers import AuthSerializer, KeySerializer
 from base.perms import IsAdminAPI
+
+import json
 
 
 def index(request):
     return render(request, "build/index.html")
 
+class AuthsAPI(APIView):
+    permission_classes = (IsAdminAPI,)
+
+    def get(self, request):
+        query = Auth.objects.all().values()
+        return Response(query, status=HTTP_200_OK)
+    
+    def post(self, request):
+        auth = AuthSerializer(data=request.data)
+        if not auth.is_valid():
+            return Response({"result","Auth object is not valid"}, status=HTTP_400_BAD_REQUEST)
+        else:
+            auth.save()
+            return Response({}, status=HTTP_200_OK)
+    
+    def delete(self, request):
+        Auth.objects.all().delete()
+        return Response({},status=HTTP_200_OK)
+    
+class AuthAPI(APIView):
+    permission_classes = (IsAdminAPI,)
+
+    def get(self, request, auth_id):
+        try:
+            query = Auth.objects.all().values().filter(id=auth_id).get()
+        except ObjectDoesNotExist:
+            return Response({}, status=HTTP_404_NOT_FOUND)
+        return Response(query, status=HTTP_200_OK)
+
+    def put(self, request, auth_id):
+        if not AuthSerializer(data=request.data).is_valid():
+            return Response({"result": "Auth object is not valid"}, status=HTTP_400_BAD_REQUEST)
+        else:
+            try:
+                auth = Auth.objects.all().filter(id=auth_id).get()
+            except ObjectDoesNotExist:
+                return Response({}, status=HTTP_404_NOT_FOUND)
+            for key, value in request.data.items():
+                setattr(auth, key, value)
+            auth.save()
+            return Response({}, status=HTTP_200_OK)
+
+    def delete(self, request, auth_id):
+        Auth.objects.all().filter(id=auth_id).delete()
+        return Response({}, status=HTTP_200_OK)
+
+class KeysAPI(APIView):
+    permission_classes = (IsAdminAPI,)
+
+    def get(self, request):
+        query = Key.objects.all().values()
+        return Response(query, status=HTTP_200_OK)
+    
+    def post(self, request):
+        key = KeySerializer(data=request.data)
+        if not key.is_valid():
+            return Response({"result","Key object is not valid"}, status=HTTP_400_BAD_REQUEST)
+        else:
+            key.save()
+            return Response({}, status=HTTP_200_OK)
+    
+    def delete(self, request):
+        Key.objects.all().delete()
+        return Response({},status=HTTP_200_OK)
+
+class KeyAPI(APIView):
+    permission_classes = (IsAdminAPI,)
+
+    def get(self, request, key_id):
+        try:
+            query = Key.objects.all().values().filter(id=key_id).get()
+        except ObjectDoesNotExist:
+            return Response({}, status=HTTP_404_NOT_FOUND)
+        return Response(query, status=HTTP_200_OK)
+
+    def put(self, request, key_id):
+        if not KeySerializer(data=request.data).is_valid():
+            return Response({"result": "User object is not valid"}, status=HTTP_400_BAD_REQUEST)
+        else:
+            try:
+                keym = Key.objects.all().filter(id=key_id).get()
+            except ObjectDoesNotExist:
+                return Response({}, status=HTTP_404_NOT_FOUND)
+            for key, value in request.data.items():
+                setattr(keym, key, value)
+            keym.save()
+            return Response({}, status=HTTP_200_OK)
+
+    def delete(self, request, key_id):
+        Key.objects.all().filter(id=key_id).delete()
+        return Response({}, status=HTTP_200_OK)
 
 class UsersAPI(APIView):
     permission_classes = (IsAdminAPI,)
