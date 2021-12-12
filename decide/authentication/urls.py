e3d54a754c29d7f4ddc4a42641c2f93b7cb76202
carvilgar1 <carvilgar1@alum.us.es>
2021-12-12 14:25:56 +0100

    <feature>: <login> INICIO DE SESIÓN CON CLIENTE LDAP IMPLEMENTADO
    Ahora se puede iniciar sesión con las credenciales de un cliente LDAP.
    Este incremento sólo incluye el backend y sólo se puede iniciar sesión desde clientes Postman o similares a trevés
    del endpoint http://localhost:8000/authentication/loginLDAP/

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view()),
     path('getuser/', GetUserView.as_view()),
     path('register/', RegisterView.as_view()),
+    path('loginLDAP/', LDAPLogin.as_view()),
 ]
--- decide/local_settings.example.py
+++ decide/local_settings.example.py
@@ -1,3 +1,6 @@
+import ldap
+from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
+
 ALLOWED_HOSTS = ["*"]
 
 # Modules in use, commented modules that you won't use
@@ -39,3 +42,29 @@ DATABASES = {
 
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
+
+# Baseline configuration.
+AUTH_LDAP_SERVER_URI = 'ldap://:389'
+
+AUTH_LDAP_BIND_DN = 'cn=admin,dc=decide,dc=org'
+AUTH_LDAP_BIND_PASSWORD = 'decide'
+AUTH_LDAP_USER_SEARCH = LDAPSearch(
+    'ou=people,dc=decide,dc=world',
+    ldap.SCOPE_SUBTREE,
+    '(uid=%(user)s)',
+)
+
+# Populate the Django user from the LDAP directory.
+AUTH_LDAP_USER_ATTR_MAP = {
+    'first_name': 'cn',
+    'last_name': 'sn',
+    'email': 'mail',
+}
+
+# Keep ModelBackend around for per-user permissions and maybe a local
+# superuser.
+
+AUTHENTICATION_BACKENDS = [
+    'django_auth_ldap.backend.LDAPBackend',
+]
+
