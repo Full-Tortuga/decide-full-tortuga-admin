a7dc61ae5890ff47955df3f941e077ab98ac7c08
Fernando <frabascol@gmail.com>
2021-12-14 15:34:40 +0100

    #16 - fix: change operation url and added bulk delete

--- decide/administration/frontend/src/api/userApiUtils.ts
+++ decide/administration/frontend/src/api/userApiUtils.ts
@@ -2,13 +2,16 @@ import { axios } from "api/config";
 
 const userApi = {
   // bulk operations
-  getUsers: () => axios.get("/users"),
+  getUsers: () => axios.get("api/users"),
+  //createUsers: (user: any) => axios.post("/users/", user),
+  deleteUsers: () => axios.delete("api/users"),
 
   // individual operations
-  getUser: (id: string) => axios.get(`/users/${id}`),
-  createUser: (user: any) => axios.post("/users", user),
-  updateUser: (user: any) => axios.put(`/users/${user.id}`, user),
-  deleteUser: (id: string) => axios.delete(`/users/${id}`),
+  getUser: (id: string) => axios.get(`api/users/${id}`),
+  createUser: (user: any) => axios.post("api/users", user),
+  updateUser: (user: any) => axios.put(`api/users/${user.id}`, user),
+  deleteUser: (id: string) => axios.delete(`api/users/${id}`),
 };
+
 
 export default userApi;
