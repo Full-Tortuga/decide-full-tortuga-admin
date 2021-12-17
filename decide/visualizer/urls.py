b4c48d725b3563e764a09ada65e4c52087efc5c3
alejandropiuryp <alejandropiuryp@gmail.com>
2021-12-17 11:03:53 +0100

    feature-Datos CSV - 001
    Se han modificado los archivos views.py, urls.py y visualizer-html del modulo de visualizer, para añadir en view la nueva clase que permite la exportacion de lo datos a CSV.

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -4,5 +4,5 @@ from .views import VisualizerView, Votes_csv
 
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
-   path('votes_csv/<int:voting_id>', Votes_csv.as_view()),
+    path('votes/<int:voting_id>/', Votes_csv.as_view()),
 ]
--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -9,14 +9,32 @@ from base import mods
 from collections import OrderedDict
 
 import json
+import csv
 
 
 
 #Generate a CSV File 
 class Votes_csv(View):
     def get(self,request,*args,**kwargs):
+        vid = kwargs.get('voting_id', 0)
+        try:
+            r = mods.get('voting', params={'id':vid})
+            dataList =eval(r[0]['postproc'])
+            voting = []
+            for e in dataList:
+                voting.append(dict(e))
+        except:
+            raise Http404
 
-        return response
+        res = response.HttpResponse(content_type="text/csv")
+        res['Content-Disposition'] = 'attachment; filename=' + str(r[0]["id"]) + '.csv'
+
+        csv_file = csv.writer(res)
+
+        csv_file.writerow(["Opcion", "Puntuacion", "Votos"])
+        for vote in voting:
+            csv_file.writerow([vote["option"], vote["postproc"], vote["votes"]])
+        return res
 
 class VisualizerView(TemplateView):
     template_name = 'visualizer/visualizer.html'
