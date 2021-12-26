4c642189f43b45bfd42e54e8d8e56ae0e4cd7b45
pedalopon <pedropontiga@gmail.com>
2021-12-26 19:30:42 +0100

    #38-fix:Fixed updateUser validation

--- decide/administration/serializers.py
+++ decide/administration/serializers.py
@@ -13,3 +13,11 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password')
+
+
+class UserUpdateSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150, min_length=1)
+    first_name = serializers.CharField(max_length=30)
+    last_name = serializers.CharField(max_length=150)
+    password = serializers.CharField(allow_blank=True)
+    email = serializers.EmailField()
--- decide/utils/utils.py
+++ decide/utils/utils.py
@@ -1,10 +1,6 @@
 from rest_framework.exceptions import ValidationError
 
 
-def get_ids(ids):
-    return [i for i in ids.split(",") if str.isdigit(i)]
-
-
 def is_valid(expression, msg):
     if not expression:
         raise ValidationError(msg)
