cfb362ee06fd8ea5bbdc174ce6759528f7ea21f0
Marta Sampedro Bernal <sbmarta2000@gmail.com>
2021-12-30 15:57:50 +0100

    Password hashed in post

--- decide/authentication/serializers.py
+++ decide/authentication/serializers.py
@@ -5,9 +5,35 @@ from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            email=validated_data['email'],
+            password=make_password(validated_data['password'])  # here
+        )
+        return user
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.first_name = validated_data.get(
+            'first_name', instance.first_name)
+        instance.last_name = validated_data.get(
+            'last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.password = validated_data.get(
+            'password', make_password(instance.password))
+
+        instance.save()
+        return instance
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password', 'is_active', 'is_superuser', 'is_staff')
-        read_only_fields = ('is_active','is_superuser')
-        extra_kwargs = {'password': {'write_only': True, 'min_length': 4, 'required':True}}
+        fields = ('id', 'username', 'first_name', 'last_name', 'email',
+                  'password', 'is_active', 'is_superuser', 'is_staff')
+        read_only_fields = ('is_active', 'is_superuser', 'is_staff')
+        extra_kwargs = {'password': {
+            # 'write_only': True,
+            'min_length': 4, 'required': True}}
+    validate_password = make_password
