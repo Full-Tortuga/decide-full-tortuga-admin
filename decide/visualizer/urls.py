6df6c1d831aa4842c2a7ecd25221164443d89935
JoseCarlosMorales <josmorbor3@alum.us.es>
2021-12-30 12:33:22 +0100

    feature-Graficos-003
    The implementation of the graphs and the download of binary voting data has been carried out.

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import VisualizerView, Votes_csv
+from .views import VisualizerView, Votes_csv, VisualizerViewBinary, VotesBinary_csv
 
-
+app_name= 'visualizer'
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
     path('votes/<int:voting_id>/', Votes_csv.as_view()),
+    path('binaryVoting/<int:voting_id>/', VisualizerViewBinary.as_view()),
+    path('votes/binaryVoting/<int:voting_id>/', VotesBinary_csv.as_view())
 ]
--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -8,8 +8,12 @@ from django.views.generic.base import View
 from base import mods
 from collections import OrderedDict
 
+from django.shortcuts import get_object_or_404
+from voting.models import BinaryVoting
+
 import json
 import csv
+import os
 
 
 
@@ -54,3 +58,35 @@ class VisualizerView(TemplateView):
             raise Http404
 
         return context
+
+
+class VisualizerViewBinary(TemplateView):
+    template_name = 'visualizer/visualizer_binary.html'
+
+    def get_context_data(self,voting_id, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            voting = get_object_or_404(BinaryVoting,pk=voting_id)
+            context['voting'] = json.dumps(BinaryVoting.toJson(voting))
+        except:
+            raise Http404
+
+        return context
+
+class VotesBinary_csv(View):
+    def get(self,request,voting_id,*args,**kwargs):
+        try:
+            voting = get_object_or_404(BinaryVoting,pk=voting_id)
+        except:
+            raise Http404
+
+        res = response.HttpResponse(content_type="text/csv")
+        res['Content-Disposition'] = 'attachment; filename=' + str(voting.id) + '.csv'
+
+        csv_file = csv.writer(res)
+
+        csv_file.writerow(["Opcion", "Puntuacion", "Votos"])
+
+        for vote in voting.postproc:
+            csv_file.writerow([vote["option"], vote["postproc"], vote["votes"]])
+        return res
