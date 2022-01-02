de08c0999e9d30f4e50789ff4edf66b22ba6a1d5
JJosePP <jjperezplata@gmail.com>
2022-01-02 20:06:32 +0100

    feature-voting_implementation-007
    Add new binaryVoting and multipleVoting views to allow the Discord
    bot to query voting data.

--- decide/voting/serializers.py
+++ decide/voting/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Question
+from .models import BinaryQuestion, BinaryQuestionOption, BinaryVoting, MultipleQuestion, MultipleQuestionOption, MultipleVoting, Question
 from .models import QuestionOption
 from .models import Voting
 from base.serializers import KeySerializer, AuthSerializer
@@ -27,7 +27,7 @@ class VotingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Voting
         fields = ('id', 'name', 'desc', 'question', 'start_date',
-                  'end_date', 'pub_key', 'auths', 'tally', 'postproc')
+                  'end_date', 'pub_key', 'auths', 'tally', 'postproc', 'type')
 
 
 class SimpleVotingSerializer(serializers.HyperlinkedModelSerializer):
@@ -35,6 +35,61 @@ class SimpleVotingSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Voting
-        fields = ('name', 'desc', 'question', 'start_date', 'end_date')
+        fields = ('name', 'desc', 'question', 'start_date', 'end_date', 'type')
 
 
+class BinaryQuestionOptionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = BinaryQuestionOption
+        fields = ('number', 'option')
+
+class BinaryQuestionSerializer(serializers.HyperlinkedModelSerializer):
+    options = BinaryQuestionOptionSerializer(many=True)
+    class Meta:
+        model = BinaryQuestion
+        fields = ('desc', 'options')
+
+class BinaryVotingSerializer(serializers.HyperlinkedModelSerializer):
+    question = BinaryQuestionSerializer(many=False)
+    pub_key = KeySerializer()
+    auths = AuthSerializer(many=True)
+
+    class Meta:
+        model = BinaryVoting
+        fields = ('id', 'name', 'desc', 'question', 'start_date',
+                  'end_date', 'pub_key', 'auths', 'tally', 'postproc', 'type')
+
+class SimpleBinaryVotingSerializer(serializers.HyperlinkedModelSerializer):
+    question = BinaryQuestionSerializer(many=False)
+
+    class Meta:
+        model = BinaryVoting
+        fields = ('name', 'desc', 'question', 'start_date', 'end_date', 'type')
+
+class MultipleQuestionOptionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = MultipleQuestionOption
+        fields = ('number', 'option')
+
+class MultipleQuestionSerializer(serializers.HyperlinkedModelSerializer):
+    options = MultipleQuestionOptionSerializer(many=True)
+    class Meta:
+        model = MultipleQuestion
+        fields = ('desc', 'options')
+
+class MultipleVotingSerializer(serializers.HyperlinkedModelSerializer):
+    question = MultipleQuestionSerializer(many=False)
+    pub_key = KeySerializer()
+    auths = AuthSerializer(many=True)
+
+    class Meta:
+        model = MultipleVoting
+        fields = ('id', 'name', 'desc', 'question', 'start_date',
+                  'end_date', 'pub_key', 'auths', 'tally', 'postproc', 'type')
+
+class SimpleMultipleVotingSerializer(serializers.HyperlinkedModelSerializer):
+    question = MultipleQuestionSerializer(many=False)
+
+    class Meta:
+        model = MultipleVoting
+        fields = ('name', 'desc', 'question', 'start_date', 'end_date', 'type')
--- decide/voting/urls.py
+++ decide/voting/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.VotingView.as_view(), name='voting'),
     path('<int:voting_id>/', views.VotingUpdate.as_view(), name='voting'),
+    path('binaryVoting/', views.BinaryVotingView.as_view(), name='binaryVoting'),
+    path('binaryVoting/<int:voting_id>/', views.BinaryVotingUpdate.as_view(), name='binaryVoting'),
+    path('multipleVoting/', views.MultipleVotingView.as_view(), name='multipleVoting'),
+    path('multipleVoting/<int:voting_id>/', views.MultipleVotingUpdate.as_view(), name='multipleVoting')
 ]
