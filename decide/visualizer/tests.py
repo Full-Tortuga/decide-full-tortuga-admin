f04e31b7ac20f60d623ca12ef763e072a686d47c
JoseCarlosMorales <josmorbor3@alum.us.es>
2022-01-02 18:37:20 +0100

    feature/Graficos-003 | Test of Multiple Voting
    Carrying out the implementation of the unit tests corresponding to multiple voting.

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -13,7 +13,7 @@ from census.models import Census
 from mixnet.mixcrypt import ElGamal
 from mixnet.mixcrypt import MixCrypt
 from mixnet.models import Auth
-from voting.models import Voting, Question, QuestionOption, ScoreVoting, ScoreQuestion, ScoreQuestionOption
+from voting.models import BinaryQuestionOption, MultipleQuestionOption, Voting, Question, QuestionOption, ScoreVoting, ScoreQuestion, ScoreQuestionOption, BinaryVoting, BinaryQuestion, MultipleVoting, MultipleQuestion
 
 from django.test import Client
 
@@ -126,17 +126,66 @@ class VisualizerTestCase(BaseTestCase):
                 self.login(user=user.username)
                 voter = voters.pop()
                 self.client.post('/store/', data, format='json')
+    
+
+    def create_multiple_voting(self):
+
+        #Create Voting
+        q = MultipleQuestion(desc='test question')
+        q.save()
+        for i in range(5):
+            opt = MultipleQuestionOption(question=q, option='option {}'.format(i+1))
+            opt.save()
+        v = MultipleVoting(id=101,name='test voting', question=q)
+
+        a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
+                                          defaults={'me': True, 'name': 'test auth'})
+        a.save()
+        v.auths.add(a)
+        v.create_pubkey()
+        v.start_date = timezone.now()
+        v.save()
+
+        self.login()  # set token
+        v.tally_votes(self.token)
+
+        #Create Voters
+        for i in range(50):
+            u, _ = User.objects.get_or_create(username='testvoter{}'.format(i))
+            u.is_active = True
+            u.save()
+            c = Census(voter_id=u.id, voting_id=v.id)
+            c.save()
+
+        #Store Votes 
+        voters = list(Census.objects.filter(voting_id=v.id))
+        voter = voters.pop()
+
+        for opt in v.question.options.all():
+            for i in range(random.randint(0, 5)):
+                a, b = self.encrypt_msg(opt.number, v)
+                data = {
+                    'voting': v.id,
+                    'voter': voter.voter_id,
+                    'vote': { 'a': a, 'b': b },
+                    'type': 'MV'
+                }
+                user = self.get_or_create_user(voter.voter_id)
+                self.login(user=user.username)
+                voter = voters.pop()
+                mods.post('store', json=data)
 
     def setUp(self):
         super().setUp()
         self.create_simple_voting()
         self.create_scoring_voting()
+        self.create_multiple_voting()
         
         
     def tearDown(self):
         super().tearDown()
 
-    def test_visualizer_voting(self):
+    def test_visualizer_scoring_voting(self):
         response = self.client.get('/visualizer/scoringVoting/{}/'.format(101))
         self.assertEqual(response.status_code,200)
         response = self.client.get('/visualizer/votes/scoryVoting/{}/'.format(101))
@@ -147,5 +196,10 @@ class VisualizerTestCase(BaseTestCase):
         self.assertEqual(response.status_code,200)
         response = self.client.get('/visualizer/votes/{}/'.format(101))
         self.assertEqual(response['content-type'],'text/csv')
-
+      
+    def test_visualizer_multiple_voting(self):
+        response = self.client.get('/visualizer/{}/'.format(101))
+        self.assertEqual(response.status_code,200)
+        response = self.client.get('/visualizer/votes/multipleVoting/{}/'.format(101))
+        self.assertEqual(response['content-type'],'text/csv')
   
