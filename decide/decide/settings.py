f4591f5a2a96ddc9788f83d07cc51fd9aa198e62
alejandropiuryp <alejandropiuryp@gmail.com>
2022-01-04 01:03:31 +0100

    Update settings.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -208,5 +208,9 @@ if os.path.exists("config.jsonnet"):
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
 
+NOSE_ARGS = [
+    '--with-xunit'
+]
+
 import django_heroku
-django_heroku.settings(locals())
+django_heroku.settings(locals(), test_runner=false)
