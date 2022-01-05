6d7cedab722806f7b9a677c17ef337fd18c998ff
alvechdel <alvechdel@alum.us.es>
2022-01-05 01:18:04 +0100

    hotfix-Heroku Local Set Up finished
    Fix bug regarding url for localhost

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -16,18 +16,18 @@ CORS_ORIGIN_ALLOW_ALL = True
 ]'''
 
 APIS = {
-    'authentication': 'http://localhost:8000/',
-    'base': 'http://localhost:8000/',
-    'booth': 'http://localhost:8000/',
-    'census': 'http://localhost:8000/',
-    'mixnet': 'http://localhost:8000/',
-    'postproc': 'http://localhost:8000/',
-    'store': 'http://localhost:8000/',
-    'visualizer': 'http://localhost:8000/',
-    'voting': 'http://localhost:8000/',
+    'authentication': 'http://localhost:8000',
+    'base': 'http://localhost:8000',
+    'booth': 'http://localhost:8000',
+    'census': 'http://localhost:8000',
+    'mixnet': 'http://localhost:8000',
+    'postproc': 'http://localhost:8000',
+    'store': 'http://localhost:8000',
+    'visualizer': 'http://localhost:8000',
+    'voting': 'http://localhost:8000',
 }
 
-BASEURL = 'http://localhost:8000/'
+BASEURL = 'http://localhost:8000'
 
 DATABASES = {
     'default': {
