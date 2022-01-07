0c7672cf0ad640c5bd49db34882e19998ac776e7
Jose Nieves <esoj.nieves@gmail.com>
2022-01-07 14:12:59 +0100

    #65-feat: implement notification component

--- decide/administration/frontend/src/components/01-atoms/index.ts
+++ decide/administration/frontend/src/components/01-atoms/index.ts
@@ -2,5 +2,6 @@ import { Button, IconButton } from "./Button";
 import { Loader } from "./Loader";
 import { Title } from "./Title";
 import { Input } from "./Input";
+import { Notification } from "./Notification";
 
-export { Button, IconButton, Loader, Title, Input };
+export { Button, IconButton, Loader, Title, Input, Notification };
