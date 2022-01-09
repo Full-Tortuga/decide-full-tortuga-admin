4ed9107d651b28f27e81f22ed27c0db840bfba27
Jose Nieves <esoj.nieves@gmail.com>
2022-01-09 18:54:37 +0100

    ci: add remote mongo database

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -154,10 +154,13 @@ WSGI_APPLICATION = 'decide.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
-        'NAME': 'decide',
-        'CLIENT': {
-            'host': '127.0.0.1',
-        }
+        "CLIENT": {
+            "name": 'decide',
+            "host": 'mongodb+srv://decide:@decide.3vypb.mongodb.net/decide?retryWrites=true&w=majority',
+            "username": 'decide',
+            "password": 'decide',
+            "authMechanism": "SCRAM-SHA-1",
+        },
     }
 }
 
